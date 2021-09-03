7806b99e4877c5df13c5a00c1e7acc9214a27b5b
Clara BOUCHER <clbouche@e1r11p23.42.fr>
2021-09-03 13:42:01 +0200

    orga

--- srcs/parsing/init.c
+++ srcs/parsing/init.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: clbouche <clbouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/24 15:26:52 by clbouche          #+#    #+#             */
-/*   Updated: 2021/08/30 12:21:46 by clbouche         ###   ########.fr       */
+/*   Created: 2021/08/30 12:20:54 by clbouche          #+#    #+#             */
+/*   Updated: 2021/08/30 12:21:45 by clbouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "minishell.h"
 
-int main(int argc, char **argv, char **env)
+int	init()
 {
-	char *line;
-
-	if (argc == 1)
-	{
-		//initialisation
-		while (1)
-		{
-			//faire le parsing de la ligne envoye
-			ft_putstr_fd("minishell$  ", 0);
-			get_next_line(0, &line);
-		}
-	}
-	//fonctions de free
-	return (0);
+	
 }
--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.c                                        :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: clbouche <clbouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/24 15:26:52 by clbouche          #+#    #+#             */
-/*   Updated: 2021/08/30 12:21:46 by clbouche         ###   ########.fr       */
+/*   Created: 2021/08/30 12:20:02 by clbouche          #+#    #+#             */
+/*   Updated: 2021/08/30 12:21:44 by clbouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "minishell.h"
 
-int main(int argc, char **argv, char **env)
+void	parser(char **line)
 {
-	char *line;
-
-	if (argc == 1)
+	while (line)
 	{
-		//initialisation
-		while (1)
-		{
-			//faire le parsing de la ligne envoye
-			ft_putstr_fd("minishell$  ", 0);
-			get_next_line(0, &line);
-		}
+		
 	}
-	//fonctions de free
-	return (0);
 }
