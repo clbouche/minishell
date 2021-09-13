6e84e62af580abbba0d90611617932c463064246
Clara BOUCHER <clbouche@e1r9p19.42.fr>
2021-09-13 11:24:20 +0200

    dauphine + split

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,18 +6,33 @@
 /*   By: clbouche <clbouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 12:20:02 by clbouche          #+#    #+#             */
-/*   Updated: 2021/09/10 11:01:01 by clbouche         ###   ########.fr       */
+/*   Updated: 2021/09/13 11:23:57 by clbouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+/*PRINTF A SUPPR*/
+
+char	**put_token(char **line)
+{
+	int i;
+
+	i = -1;
+	while (line[i++] != NULL)
+	{
+		printf("line[%d] = : %s\n", i, line[i]);
+	}
+}
+
 void	parser(char *line, t_dlist *list)
 {
+	char **split_line;
+
 	if (line)
 	{
-
-		printf("%s\n", line);
+		split_line = ft_split(line, ' ');
+		put_token(split_line);
 	}
 	else 
 		exit(0);
