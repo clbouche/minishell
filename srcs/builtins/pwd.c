498234c8125d8a025d8d57757801d0f73b901466
Clara Boucher <clbouche@student.42.fr>
2021-09-13 17:37:49 +0200

    pwd

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,11 +6,16 @@
 /*   By: claclou <claclou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 12:25:32 by clbouche          #+#    #+#             */
-/*   Updated: 2021/09/13 16:34:29 by claclou          ###   ########.fr       */
+/*   Updated: 2021/09/13 17:37:32 by claclou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+/*
+** writes the absolute pathname of the current working directory 
+** to the standard output.
+*/ 
 
 void	pwd()
 {
