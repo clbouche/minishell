bc993ca15c424e544de61528469c551e491d9887
ldes-cou <ldes-cou@student.fr>
2021-10-07 15:45:44 +0200

    exec_simple

--- obj/execution/exec_simple.d
+++
@@ -1,5 +0,0 @@
-obj/execution/exec_simple.o: srcs/execution/exec_simple.c includes/minishell.h \
-  includes/libft/includes/libft.h
-srcs/execution/exec_simple.c:
-includes/minishell.h:
-includes/libft/includes/libft.h:
