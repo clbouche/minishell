b82215e538b76b0000b809e07ac21409fb2ecae4
ldes-cou <ldes-cou@student.fr>
2021-10-18 16:30:49 +0200

    without obj

--- obj/builtins/ft_pwd.d
+++
@@ -1,5 +0,0 @@
-obj/builtins/ft_pwd.o: srcs/builtins/ft_pwd.c includes/minishell.h \
-  includes/libft/includes/libft.h
-srcs/builtins/ft_pwd.c:
-includes/minishell.h:
-includes/libft/includes/libft.h:
