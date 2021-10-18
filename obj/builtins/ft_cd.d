9bd36dabe3161665e49ca321c9fa43abbce4aa53
ldes-cou <ldes-cou@student.fr>
2021-10-18 15:27:43 +0200

    exit fixed

--- obj/builtins/ft_cd.d
+++
@@ -1,5 +0,0 @@
-obj/builtins/ft_cd.o: srcs/builtins/ft_cd.c includes/minishell.h \
-  includes/libft/includes/libft.h
-srcs/builtins/ft_cd.c:
-includes/minishell.h:
-includes/libft/includes/libft.h:
