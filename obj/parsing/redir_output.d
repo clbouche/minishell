6388478952e21867d34fb2c043d3991ef23564f7
Lucrèce des Courtis <lucrece@MacBook-Pro-de-Lucrece.local>
2021-10-19 14:55:35 +0200

    merging master

--- obj/parsing/redir_output.d
+++
@@ -1,6 +0,0 @@
-obj/parsing/redir_output.o: srcs/parsing/redir_output.c \
-  srcs/parsing/../../includes/minishell.h \
-  includes/libft/includes/libft.h
-srcs/parsing/redir_output.c:
-srcs/parsing/../../includes/minishell.h:
-includes/libft/includes/libft.h:
