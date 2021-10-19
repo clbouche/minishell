f5b7884501c41b05315fc99e6158f847f98cfec3
Lucrèce des Courtis <lucrece@MacBook-Pro-de-Lucrece.local>
2021-10-19 14:55:25 +0200

    merging master

---
+++ obj/builtins/is_builtins.d
@@ -0,0 +1,5 @@
+obj/builtins/is_builtins.o: srcs/builtins/is_builtins.c includes/minishell.h \
+  includes/libft/includes/libft.h
+srcs/builtins/is_builtins.c:
+includes/minishell.h:
+includes/libft/includes/libft.h:
