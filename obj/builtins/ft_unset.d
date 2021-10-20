06d6b0a162ca7b9f4879e4e99e98ca47d017b7c4
ldes-cou <ldes-cou@student.fr>
2021-10-20 18:20:39 +0200

    exec

---
+++ obj/builtins/ft_unset.d
@@ -0,0 +1,5 @@
+obj/builtins/ft_unset.o: srcs/builtins/ft_unset.c includes/minishell.h \
+  includes/libft/includes/libft.h
+srcs/builtins/ft_unset.c:
+includes/minishell.h:
+includes/libft/includes/libft.h:
