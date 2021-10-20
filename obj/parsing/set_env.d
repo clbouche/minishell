7a45a03efce58476cda51eb8410dbf74e69e1bb8
ldes-cou <ldes-cou@student.fr>
2021-10-20 18:22:54 +0200

    lol

---
+++ obj/parsing/set_env.d
@@ -0,0 +1,5 @@
+obj/parsing/set_env.o: srcs/parsing/set_env.c includes/minishell.h \
+  includes/libft/includes/libft.h
+srcs/parsing/set_env.c:
+includes/minishell.h:
+includes/libft/includes/libft.h:
---
+++ obj/parsing/variables.d
@@ -0,0 +1,5 @@
+obj/parsing/variables.o: srcs/parsing/variables.c includes/minishell.h \
+  includes/libft/includes/libft.h
+srcs/parsing/variables.c:
+includes/minishell.h:
+includes/libft/includes/libft.h:
