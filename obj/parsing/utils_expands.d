33661d45a6da1d5d2a2791ec8c9743b536bf58cf
ldes-cou <ldes-cou@student.fr>
2021-11-02 16:00:45 +0100

    ok export normal

--- obj/parsing/utils_expands.d
+++
@@ -1,5 +0,0 @@
-obj/parsing/utils_expands.o: srcs/parsing/utils_expands.c includes/minishell.h \
-  includes/libft/includes/libft.h
-srcs/parsing/utils_expands.c:
-includes/minishell.h:
-includes/libft/includes/libft.h:
