dd6dc2f5f36e7c741a97a7f3e4847b65bc5c8625
ldes-cou <ldes-cou@student.fr>
2021-11-10 12:20:50 +0100

    lol

--- obj/parsing/variables.d
+++
@@ -1,5 +0,0 @@
-obj/parsing/variables.o: srcs/parsing/variables.c includes/minishell.h \
-  includes/libft/includes/libft.h
-srcs/parsing/variables.c:
-includes/minishell.h:
-includes/libft/includes/libft.h:
