c6a8b034a318410bffcd6b4fd11549e0c337bd1f
Clara BOUCHER <clbouche@e2r7p10.clusters.42paris.fr>
2021-11-16 10:06:00 +0100

    grosse merde 2

--- obj/parsing/clean_input.d
+++
@@ -1,5 +0,0 @@
-obj/parsing/clean_input.o: srcs/parsing/clean_input.c includes/minishell.h \
-  includes/libft/includes/libft.h
-srcs/parsing/clean_input.c:
-includes/minishell.h:
-includes/libft/includes/libft.h:
--- obj/parsing/redir_utils.d
+++
@@ -1,5 +0,0 @@
-obj/parsing/redir_utils.o: srcs/parsing/redir_utils.c includes/minishell.h \
-  includes/libft/includes/libft.h
-srcs/parsing/redir_utils.c:
-includes/minishell.h:
-includes/libft/includes/libft.h:
