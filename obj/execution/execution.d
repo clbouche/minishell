bda6bb21de627a1ebb3e0fb1e00ee6f602650a00
Clara BOUCHER <clbouche@e2r7p10.clusters.42paris.fr>
2021-11-16 10:05:37 +0100

    grosse merde

---
+++ obj/execution/execution.d
@@ -0,0 +1,5 @@
+obj/execution/execution.o: srcs/execution/execution.c includes/minishell.h \
+  includes/libft/includes/libft.h
+srcs/execution/execution.c:
+includes/minishell.h:
+includes/libft/includes/libft.h:
--- sources.mk
+++ sources.mk
@@ -17,7 +17,6 @@ SOURCES		+=                            		\
 		srcs/parsing/redir_utils.c           	\
 		srcs/parsing/bool_check_utils.c      	\
 		srcs/parsing/parsing.c               	\
-		srcs/parsing/heredoc.c               	\
 		srcs/parsing/utils_expands.c         	\
 		srcs/parsing/redir.c                 	\
 
