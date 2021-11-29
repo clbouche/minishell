69468768e00cee0a8b332320166e909f9e064d54
Clara BOUCHER <clbouche@e2r8p21.clusters.42paris.fr>
2021-11-29 11:37:29 +0100

    gestions erreurs in progress

---
+++ obj/builtins/utils_builtins.d
@@ -0,0 +1,5 @@
+obj/builtins/utils_builtins.o: srcs/builtins/utils_builtins.c includes/minishell.h \
+  includes/libft/includes/libft.h
+srcs/builtins/utils_builtins.c:
+includes/minishell.h:
+includes/libft/includes/libft.h:
