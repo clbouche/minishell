e784072ce894520460ce7c57ac9fdaf1ed70eb7d
ldes-cou <ldes-cou@student.fr>
2021-12-01 18:29:05 +0100

    without obj

--- obj/builtins/ft_env.d
+++
@@ -1,5 +0,0 @@
-obj/builtins/ft_env.o: srcs/builtins/ft_env.c includes/minishell.h \
-  includes/libft/includes/libft.h
-srcs/builtins/ft_env.c:
-includes/minishell.h:
-includes/libft/includes/libft.h:
--- obj/builtins/utils_builtins.d
+++
@@ -1,5 +0,0 @@
-obj/builtins/utils_builtins.o: srcs/builtins/utils_builtins.c includes/minishell.h \
-  includes/libft/includes/libft.h
-srcs/builtins/utils_builtins.c:
-includes/minishell.h:
-includes/libft/includes/libft.h:
