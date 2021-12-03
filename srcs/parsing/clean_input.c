89426baa529988962cece96ecd6aeb4996bd110a
ldes-cou <ldes-cou@student.fr>
2021-12-03 10:15:07 +0100

    norm ok

--- srcs/parsing/clean_input.c
+++ srcs/parsing/clean_input.c
@@ -6,7 +6,7 @@
 /*   By: ldes-cou <ldes-cou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 13:48:26 by clbouche          #+#    #+#             */
-/*   Updated: 2021/12/03 10:02:20 by ldes-cou         ###   ########.fr       */
+/*   Updated: 2021/12/03 10:14:20 by ldes-cou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,24 @@ void	copy_input(char *dst, char *src)
 	*dst = '\0';
 }
 
+int	manage_quote(char *line, int i)
+{
+	char	quote;
+
+	quote = *(line++);
+	while (*line && *line != quote)
+	{
+		i++;
+		line++;
+	}
+	if (!*line)
+		return (-1);
+	return (i);
+}
+
 int	input_len(char *line)
 {
 	int		i;
-	char	quote;
 
 	i = 0;
 	while (*line)
@@ -48,14 +62,7 @@ int	input_len(char *line)
 			line++;
 		else if (*line == '"' || *line == '\'')
 		{
-			quote = *(line++);
-			while (*line && *line != quote)
-			{
-				i++;
-				line++;
-			}
-			if (!*line)
-				return (-1);
+			i = manage_quote(line, i);
 			line++;
 			i += 2;
 		}
