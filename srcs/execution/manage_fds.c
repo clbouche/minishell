7cadc81dd4025ce4adb7778923d49edbf71d987c
Clara BOUCHER <clbouche@e2r7p18.clusters.42paris.fr>
2021-12-03 10:30:00 +0100

    restore fd OK

--- srcs/execution/manage_fds.c
+++ srcs/execution/manage_fds.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   manage_fds.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldes-cou <ldes-cou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: clbouche <clbouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 11:36:04 by ldes-cou@st       #+#    #+#             */
-/*   Updated: 2021/12/02 17:42:17 by ldes-cou         ###   ########.fr       */
+/*   Updated: 2021/12/03 10:29:45 by clbouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,35 @@ void	pipe_in(t_data *d)
 	close(d->fds[0]);
 }
 
-void	restore_fds(t_data *d)
+void	restore_fds_redir(t_data *d)
 {
-	if (d->redir->r_out == true || d->redir->pipe_out == true)
+	if (d->redir->r_out == true)
 	{
 		close(d->file_out);
 		dup2(d->std_out, 1);
 		d->redir->r_out = false;
-		d->redir->pipe_out = false;
 	}
-	if (d->redir->r_in == true || d->redir->pipe_in == true)
+	if (d->redir->r_in == true)
 	{
 		close(d->file_in);
 		dup2(d->std_in, 0);
 		d->redir->r_in = false;
+	}
+}
+
+void	restore_fds(t_data *d)
+{
+	restore_fds_redir(d);
+	if (d->redir->pipe_out == true)
+	{
+		close(d->file_out);
+		dup2(d->std_out, 1);
+		d->redir->pipe_out = false;
+	}
+	if (d->redir->pipe_in == true)
+	{
+		close(d->file_in);
+		dup2(d->std_in, 0);
 		d->redir->pipe_in = false;
 	}
 	if (d->std_out != 1)
